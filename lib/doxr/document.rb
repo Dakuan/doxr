d3b18173de55a8c3e2706345a123c8babe27f021
Dakuan <dom.barker808@gmail.com>
2013-06-21 15:31:21 +0100

    require path

--- lib/doxr/document.rb
+++ lib/doxr/document.rb
@@ -2,19 +2,20 @@ class Doxr::Document
 
   attr_reader :elements
 
-  def initialize(file_path)
+  def initialize(file_path, temp)
+    @temp = temp
     @elements = []
     unzip file_path
     extract_styles
     extract_content
-    FileUtils.rm_rf('.temp/')
+    FileUtils.rm_rf(@temp)
   end
 
   private
 
   def get_doc(file_name)
     doc = false
-    path = ".temp/word/#{file_name}.xml"
+    path = "#{@temp}/word/#{file_name}.xml"
     File.open(path) { |file| doc = Nokogiri::XML(file).remove_namespaces! }
     raise "failed to open: #{path}" unless doc
     doc
@@ -35,7 +36,7 @@ class Doxr::Document
   def unzip(file_path)
     Zip::ZipFile.open(file_path) do |zip_file|
       zip_file.each do |f|
-        f_path=File.join('.temp/', f.name)
+        f_path=File.join("#{@temp}/", f.name)
         FileUtils.mkdir_p(File.dirname(f_path))
         zip_file.extract(f, f_path) unless File.exist?(f_path)
       end
